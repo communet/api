794bd625d953b5cd40800c09486e9687954c3bcb
Mihail <infast1k@mail.ru>
2025-01-25 18:22:23 +0300

    feat(infra): implement base objects for works with repo and uow (#611380)

---
+++ src/infra/repositories/base.py
@@ -0,0 +1,49 @@
+from abc import ABC
+from dataclasses import dataclass
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+@dataclass(eq=False, frozen=True)
+class BaseRepository(ABC):
+    """
+    Base repository with common attributes and methods.
+    :param _session: SQLAlchemy session for database operations.
+    """
+    _session: AsyncSession
+
+
+class BaseUoW(ABC):
+    """
+    Manages transactions and provides access to repositories.
+    :param session: SQLAlchemy session for database operations.
+    """
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def commit(self) -> None:
+        """Commit the current transaction in progress."""
+        await self._session.commit()
+
+    async def rollback(self) -> None:
+        """Rollback the current transaction in progress."""
+        await self._session.rollback()
+
+    async def __aenter__(self) -> "BaseUoW":
+        """
+        Enters the context manager.
+        :return: The UnitOfWork instance.
+        """
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        """
+        Exits the context manager.
+        :param exc_type: The type of exception (if any).
+        :param exc_val: The exception value (if any).
+        :param exc_tb: The exception traceback (if any).
+        """
+        if exc_type is not None:
+            await self.rollback()
+        else:
+            await self.commit()
