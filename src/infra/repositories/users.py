c6ee1797155809382f3f9a8a1cbbe9ea651c3865
Mihail <infast1k@mail.ru>
2025-01-26 12:08:35 +0300

    feat(infra): implement method for check user exists (#611380)

--- src/infra/repositories/users.py
+++ src/infra/repositories/users.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 from dataclasses import dataclass
 from typing import Optional
 
+from sqlalchemy import select, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.domain.entities.users import Credentials, Profile
@@ -14,6 +15,10 @@ from src.infra.repositories.base import BaseRepository, BaseUoW
 class BaseCredentialsRepository(BaseRepository):
     @abstractmethod
     async def create(self, credentials: Credentials) -> Optional[CredentialsModel]:
+        ...
+
+    @abstractmethod
+    async def check_user_exists(self, email: str, username: str) -> bool:
         ...
 
 
@@ -34,6 +39,17 @@ class CredentialsRepository(BaseCredentialsRepository):
         self._session.add(credentials_model)
         await self._session.flush()
         return credentials_model
+
+    async def check_user_exists(self, email: str, username: str) -> bool:
+        """
+        Check user exists with given email or username.
+        :return: True if user exists else False.
+        """
+        stmt = select(CredentialsModel).where(
+            or_(CredentialsModel.email == email, CredentialsModel.username == username)
+        )
+        result = await self._session.execute(stmt)
+        return result.scalar() is not None
 
 
 @dataclass(eq=False, frozen=True)
