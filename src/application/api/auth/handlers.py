d5d48a2c9167281549b59c3314a97d1bbda5a8ec
Mihail <infast1k@mail.ru>
2025-02-01 14:31:45 +0300

    feat(app): implement login handler (#611381)

--- src/application/api/auth/handlers.py
+++ src/application/api/auth/handlers.py
@@ -1,12 +1,18 @@
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from punq import Container
 
-from src.application.api.auth.schemas import RegisterResponseSchema, RegisterRequestSchema
+from src.application.api.auth.schemas import (
+	LoginRequestSchema,
+	LoginResponseSchema,
+	RegisterResponseSchema,
+	RegisterRequestSchema,
+)
 from src.application.api.schemas import ErrorSchema
 from src.domain.exceptions.base import ApplicationException
-from src.logic.commands.auth import RegisterCommand
+from src.logic.commands.auth import LoginCommand, RegisterCommand
 from src.logic.init.container import init_container
 from src.logic.init.mediator import Mediator
+
 
 router = APIRouter(tags=["Auth"])
 
@@ -37,3 +43,39 @@ async def register(
     except ApplicationException as exception:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"error": exception.message})
     return RegisterResponseSchema.from_entity(profile)
+
+
+@router.post(
+    path='/auth/login',
+    status_code=status.HTTP_200_OK,
+    description="Login exists user",
+    responses={
+        status.HTTP_200_OK: {"model": LoginResponseSchema},
+        status.HTTP_400_BAD_REQUEST: {"model": ErrorSchema}
+    },
+)
+async def login(
+    schema: LoginRequestSchema,
+    response: Response,
+    container: Container = Depends(init_container),
+) -> LoginResponseSchema:
+    mediator: Mediator = container.resolve(Mediator)
+
+    try: 
+        is_email = schema.is_email()
+
+        auth_data, *_ = await mediator.handle_command(LoginCommand(
+            username=schema.username if not is_email else None,
+            email=schema.username if is_email else None,
+            password=schema.password,
+        ))
+
+        response.set_cookie(
+            key="refresh_token",
+            value=auth_data.refresh_token,
+            max_age=auth_data.refresh_expires.seconds,
+            httponly=True,
+        )
+    except ApplicationException:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"error": "invalid credentials"})
+    return LoginResponseSchema.from_entity(auth_data)
--- src/application/api/auth/schemas.py
+++ src/application/api/auth/schemas.py
@@ -1,5 +1,9 @@
+import re
+
+from datetime import datetime
+
 from src.application.api.schemas import BaseRequestSchema, BaseResponseSchema
-from src.domain.entities.users import Profile
+from src.domain.entities.users import AuthData, Profile
 
 
 class RegisterRequestSchema(BaseRequestSchema):
@@ -25,4 +29,25 @@ class RegisterResponseSchema(BaseResponseSchema):
             username=entity.credentials.username.as_generic_type(),
             email=entity.credentials.email.as_generic_type(),
             avatar=entity.avatar,
+        )
+
+
+class LoginRequestSchema(BaseRequestSchema):
+    username: str
+    password: str
+
+    def is_email(self) -> bool:
+        pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+        return bool(re.fullmatch(pattern, self.username))
+
+
+class LoginResponseSchema(BaseResponseSchema):
+    access_token: str
+    access_expires: datetime
+
+    @classmethod
+    def from_entity(cls, entity: AuthData) -> "LoginResponseSchema":
+        return cls(
+            access_token=entity.access_token,
+            access_expires=entity.access_expires,
         )
