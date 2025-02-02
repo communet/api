3e31e660b3fe532df7c8d5e16ba434f9d2c689b5
Mihail <infast1k@mail.ru>
2025-02-02 20:32:15 +0300

    refactor(infra): add is_deleted field for chanel model (#636124)

--- src/infra/models/chanels.py
+++ src/infra/models/chanels.py
@@ -8,5 +8,6 @@ class ChanelModel(Base):
     __tablename__ = "chanels"
 
     name: Mapped[str] = mapped_column(String(255))
-    description: Mapped[str] = mapped_column(Text())
-    avatar: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(Text(), nullable=True, default=None)
+    is_deleted: Mapped[bool] = mapped_column(default=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True, default=None)
