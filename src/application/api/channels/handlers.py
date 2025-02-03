6987437ca2756c54c13550b787c6675421fa58ef
Mihail <infast1k@mail.ru>
2025-02-03 21:33:44 +0300

    feat(app): implement method for update channel (#636124)

--- src/application/api/channels/handlers.py
+++ src/application/api/channels/handlers.py
@@ -4,10 +4,15 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from punq import Container
 
 from src.application.api.auth.depends import get_current_user
-from src.application.api.channels.schemas import CreateChannelRequestSchema, CreateChannelResponseSchema
+from src.application.api.channels.schemas import (
+    CreateChannelRequestSchema,
+    CreateChannelResponseSchema,
+    UpdateChannelRequestSchema,
+    UpdateChannelResponseSchema,
+)
 from src.application.api.schemas import ErrorSchema
 from src.domain.exceptions.base import ApplicationException
-from src.logic.commands.channels import CreateChannelCommand, DeleteChannelCommand
+from src.logic.commands.channels import CreateChannelCommand, DeleteChannelCommand, UpdateChannelCommand
 from src.logic.init.container import init_container
 from src.logic.init.mediator import Mediator
 
@@ -20,7 +25,7 @@ router = APIRouter(tags=["Channels"])
     status_code=status.HTTP_201_CREATED,
     description="Create a new channel",
     responses={
-        status.HTTP_201_CREATED: {"model": CreateChannelRequestSchema},
+        status.HTTP_201_CREATED: {"model": CreateChannelResponseSchema},
         status.HTTP_400_BAD_REQUEST: {"model": ErrorSchema}
     },
 )
@@ -40,6 +45,35 @@ async def create_channel(
     except ApplicationException as exception:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"error": exception.message})
     return CreateChannelResponseSchema.from_entity(channel)
+
+
+@router.put(
+    path='/channels/{channel_id}',
+    status_code=status.HTTP_200_OK,
+    description="Update exists channel",
+    responses={
+        status.HTTP_200_OK: {"model": UpdateChannelResponseSchema},
+        status.HTTP_400_BAD_REQUEST: {"model": ErrorSchema},
+    },
+)
+async def update_channel(
+    channel_id: UUID,
+    schema: UpdateChannelRequestSchema,
+    _ = Depends(get_current_user),  # FIXME: Its need for protect route. Change to more useful depends without user
+    container: Container = Depends(init_container),
+) -> UpdateChannelResponseSchema:
+    mediator: Mediator = container.resolve(Mediator)
+
+    try:
+        channel, *_ = await mediator.handle_command(UpdateChannelCommand(
+            channel_id=channel_id,
+            name=schema.name,
+            description=schema.description,
+            avatar=schema.avatar,
+        ))
+    except ApplicationException as exception:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={"error": exception.message})
+    return UpdateChannelResponseSchema.from_entity(channel)
 
 
 @router.delete(
--- src/application/api/channels/schemas.py
+++ src/application/api/channels/schemas.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from src.application.api.schemas import BaseRequestSchema, BaseResponseSchema
 from src.domain.entities.channels import Channel
 
@@ -9,6 +10,26 @@ class CreateChannelRequestSchema(BaseRequestSchema):
 
 
 class CreateChannelResponseSchema(BaseResponseSchema):
+	name: str
+	description: str | None
+	avatar: str | None
+
+	@classmethod
+	def from_entity(cls, entity: Channel) -> "CreateChannelResponseSchema":
+		return cls(
+			name=entity.name.as_generic_type(),
+			description=entity.description,
+			avatar=entity.avatar,
+		)
+
+
+class UpdateChannelRequestSchema(BaseRequestSchema):
+    name: str | None
+    description: str | None
+    avatar: str | None
+
+
+class UpdateChannelResponseSchema(BaseResponseSchema):
 	name: str
 	description: str | None
 	avatar: str | None
